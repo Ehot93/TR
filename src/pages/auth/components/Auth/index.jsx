797da2c98bdb470024281750fb83440d38dd4618
Vlad-home <vlad3x@mail.ru>
2021-04-27 10:31:01 +0700

    'init'

---
+++ src/pages/auth/components/Auth/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'style.less';
+
+const Auth = () => {
+    return (
+    <div className='auth__wrapper'>
+        hello wrapp
+    </div>
+    )
+}
+
+export default Auth;
