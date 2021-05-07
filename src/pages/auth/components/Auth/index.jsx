a5973abfef1b1d4cb57d081b735b276204c27487
Vlad-home <vlad3x@mail.ru>
2021-05-08 03:24:50 +0700

    add styles

--- src/pages/auth/components/Auth/index.jsx
+++ src/pages/auth/components/Auth/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import 'style.less';
+import './style.css';
 
 const Auth = () => {
     return (
     <div className='auth__wrapper'>
-        hello wrapp
+        <label htmlFor="auth__email">enter email</label>
+        <input className='auth__email' id='auth__email' placeholder='email' type="text"/>
+        <label htmlFor="auth__password">enter password</label>
+        <input className='auth__password' id='auth__password' placeholder='password' type="text"/>
     </div>
     )
-}
+};
 
 export default Auth;
